51054786ee447558d77c5e563f907f8ef2165d7c
JohnsonArnek <johnsonarnekton@gmail.com>
2026-08-08 04:02:12 +0300

    Add missing app_commands import to bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 import database
